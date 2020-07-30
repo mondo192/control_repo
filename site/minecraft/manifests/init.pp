9367da54bc9acf975229067403e6fdbad22298da
Eamonn Keogh <31599023+mondo192@users.noreply.github.com>
2020-07-30 18:04:52 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -23,7 +23,7 @@ class minecraft (
   
   file {'/etc/systemd/system/mincraft.service':
     ensure => file,
-    content => epp('minecraft/templates/minecraft.service.epp', {
+    content => epp('minecraft/templates/minecraft.service', {
       install_dir => $install_dir, 
     }),
   }
