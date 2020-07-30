c83820711d3ac9c9d4f6c092282603f80ea88e79
Eamonn Keogh <31599023+mondo192@users.noreply.github.com>
2020-07-30 17:46:40 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -31,8 +31,7 @@ class minecraft (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [
-      Package['java'], 
+    require => [Package['java'], 
       File["${install_dir}/eula.txt"], 
       File['/etc/systemd/system/minecraft.service']
     ],
