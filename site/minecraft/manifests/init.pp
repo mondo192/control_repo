8fdc766c4393ceeb0560190b57395991ebc6cf03
Eamonn Keogh <31599023+mondo192@users.noreply.github.com>
2020-07-30 17:34:07 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,7 +1,7 @@
 class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
   $install_dir = '/opt/minecraft'
-  ) {
+) {
   file{"$install_dir":
     ensure => directory,
   }
@@ -31,8 +31,7 @@ class minecraft (
   service {'minecraft':
     ensure => running,
     enable => true,
-    require => [
-      Package['java'],
+    require => [Package['java'],
       File["${install_dir}/eula.txt"],
       File['/etc/systemd/system/minecraft.service']
     ],
